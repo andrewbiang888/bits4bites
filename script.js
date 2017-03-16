158d6a51b962bd28e48b0ea9fdfa59c7be873d1d
Samuel Wellander <wellander8@gmail.com>
2017-03-15 17:54:10 -0700

    lengthen chef descriptions

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 var persons = [{
   name: 'Andrew',
-  description: 'Bacon ipsum dolor amet tri-tip shoulder biltong, brisket bacon doner cupim turkey drumstick andouille.',
+  description: 'Bacon ipsum dolor amet tri-tip shoulder biltong, brisket bacon doner cupim turkey drumstick andouille. Bacon ipsum dolor amet tri-tip shoulder biltong, brisket bacon doner cupim turkey drumstick andouille.',
   photo: 'https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/14264927_1198730703506419_792765636242202081_n.jpg?oh=9f1f079ed641fc39454e2a72edecb54a&oe=596EF2CB',
   resume: '',
   title: 'Snookie'
 }, {
   name: 'Pirtle',
-  description: 'Rump cupim bacon tongue, capicola andouille sirloin beef drumstick shank brisket swine.',
+  description: 'Rump cupim bacon tongue, capicola andouille sirloin beef drumstick shank brisket swine. Bacon ipsum dolor amet tri-tip shoulder biltong, brisket bacon doner cupim turkey drumstick andouille.',
   photo: 'https://scontent-sea1-1.xx.fbcdn.net/v/t34.0-12/10634315_10154473586370562_1799877597_n.jpg?oh=b42dd51a9aa40599659d7916ff379c92&oe=58CACE27',
   resume: '',
   title: 'Bronch'
 }, {
   name: 'Sam',
-  description: 'Porchetta shankle biltong, ball tip venison tenderloin sausage bacon sirloin tail strip steak salami turkey.',
+  description: 'Porchetta shankle biltong, ball tip venison tenderloin sausage bacon sirloin tail strip steak salami turkey. Bacon ipsum dolor amet tri-tip shoulder biltong, brisket bacon doner cupim turkey drumstick andouille.',
   photo: 'https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/521848_10151843368756124_732343295_n.jpg?oh=6a267feeb0232fe05e943b1ca2723f9a&oe=596804BB',
   resume: '',
   title: 'Fizzle'
